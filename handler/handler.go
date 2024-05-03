370d6b86ed4bd508cd28a927afa397ac1fd3a647
Geazi Oliveira da Fonseca <geazioliveira@cefetmg.br>
2024-05-03 20:07:43 -0300

    add separate files for oute handlers

--- handler/handler.go
+++ handler/handler.go
@@ -5,33 +5,3 @@ import (
 
 	"github.com/gin-gonic/gin"
 )
-
-func CreateOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "Post Opening",
-	})
-}
-
-func ShowOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "Get Opening",
-	})
-}
-
-func DeleteOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "Delete Opening",
-	})
-}
-
-func UpdateOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "PUT Opening",
-	})
-}
-
-func ListOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "Get Openings",
-	})
-}
